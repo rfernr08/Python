196b84e4e333538e519c4e0d29f5e83f442cb5a6
Raul <rfernr08@estudiantes.unileon.es>
2024-03-10 14:18:15 +0100

    d

--- 3/Lab3_3.py
+++ 3/Lab3_3.py
@@ -1,16 +1,19 @@
 import numpy as np
 from scipy.spatial import distance
 
-# Generate a 10x4 matrix with random values between -10 and 10
-matrix = np.random.uniform(-10, 10, (10, 4))
+rows = int(input("X dimension for the matrix: "))
+cols = int(input("Y dimension for the matrix: "))
 
-# a. Create a Euclidean distance matrix
+# Generate a 10x4 matrix with random values between -10 and 10
+matrix = np.random.uniform(-10, 10, (rows, cols))
+
+# Create a Euclidean distance matrix
 dist_matrix = distance.cdist(matrix, matrix, 'euclidean')
 
 print("Euclidean distance matrix:")
 print(dist_matrix)
 
-# b. Indicate the distance between the points, when such distance is lower than 10
+# Indicate the distance between the points, when such distance is lower than 10
 rows, cols = np.where((dist_matrix > 0) & (dist_matrix < 10))
 for i, j in zip(rows, cols):
     print(f"The Euclidean distance between vectors {i} and {j} is {dist_matrix[i, j]}")
